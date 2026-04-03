711c51b3daa200688a81af6fb76d0460486a1663
Arad Manaviparast <44674146+Arad-Manavi@users.noreply.github.com>
2026-04-03 15:29:29 -0400

    fix sw.js

--- docs/sw.js
+++ docs/sw.js
@@ -1,5 +1,9 @@
 const CACHE = "translator-v1";
-const ASSETS = ["/", "/index.html", "/manifest.json"];
+const ASSETS = [
+  "/translator-app/",
+  "/translator-app/index.html",
+  "/translator-app/manifest.json"
+];
 
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
